902376e89af6b14dd274c744e8ab09172715a885
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-14 13:19:11 +0300

    pareigu lentele

--- parduotuves_lenteliu_sukurimas.sql
+++ parduotuves_lenteliu_sukurimas.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS kategorija(
 -- pridedu aprašymo stulpelį į kategoriją
 ALTER TABLE kategorija
 ADD COLUMN aprasymas VARCHAR(300)
+
+-- pareigų lentelė
+CREATE TABLE IF NOT EXISTS pareigos (
+    id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    pavadinimas VARCHAR(50) NOT NULL,
+    bonusas DECIMAL(6,2) DEFAULT 0,
+    PRIMARY KEY(id)
+) ENGINE = INNODB;
+
